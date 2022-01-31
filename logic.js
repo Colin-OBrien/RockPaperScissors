c80f163dd7ccfa37f80d5bba5cd62992c8bbac09
Colin <colin.obrien590@gmail.com>
2022-01-30 21:17:27 -0500

    added player choice by clicking

--- logic.js
+++ logic.js
@@ -1,13 +1,12 @@
-/** LOGIC TO DETERMINE HOW THE GAME WILL OPERATE */
+// LOGIC TO DETERMINE HOW THE GAME WILL OPERATE 
 
+//This function runs the computers choice
 function computerPlay() {
     let choices = ['Rock','Paper','Scissors'];
     let randomNum = Math.floor(Math.random()*3);
     
     return choices[randomNum]
 }
-
-
 
 function gameRound (player, computerPlay) {
     
@@ -34,7 +33,7 @@ function gameRound (player, computerPlay) {
         } else if (computerPlay.toLowerCase() == 'rock') {
             return (`You lose. ${computerPlay.toUpperCase()} beats ${player.toUpperCase()}!`)
             
-        } else if (computerPlay.toLowerCase() == 'paper') {
+        } else if (computerPlay.toLowerCase() == 'scissors') {
             return ('You tied')
         } else {
             return ('What?')
@@ -56,39 +55,41 @@ function gameRound (player, computerPlay) {
 
 }
 
+//This code is selecting all the buttons and for each button we need to listen for a click, 
+//when a click occurs assign player choice to the class name and log the choice
 
 function game() {
     
-    player_score = 0
-    computer_score = 0
+    let player_score = 0
+    let computer_score = 0
 
-    let keepGoing = true
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerChoice = button.className;
+            console.log(`You played ${playerChoice}!`)
+                
+            round = gameRound(playerChoice , computerPlay)
+            console.log(round)
 
-    while (keepGoing) {
+            if (round[4] == 'W') {
+                player_score += 1
+                console.log(`The score is:\nPlayer: ${player_score} \nComputer: ${computer_score}`)
+            } else if (round[4] == 'l') {
+                computer_score += 1
+                console.log(`The score is:\nPlayer: ${player_score} \nComputer: ${computer_score}`)
+            } else {
+                console.log(`The score remains:\nPlayer: ${player_score} \nComputer: ${computer_score}`)
+            }
 
-        let player_choice = prompt("Rock, Paper, or Scissors? ")
+            if (player_score == 5) {
+                console.log('You have won it all!')
+            } else if (computer_score == 5) {
+                console.log('You have lost it all!')
+            }
+            });
+    });
+    
+};
 
-        round = gameRound(player_choice, computerPlay)
-        console.log(round)
-
-        if (round[4] == 'W') {
-            player_score += 1
-        } else if (round[4] == 'l') {
-            computer_score += 1
-        } else {}
-
-        console.log(`The score is now:\nPlayer: ${player_score} \nComputer: ${computer_score}`)
-
-        if (player_score == 3) {
-            console.log("Player has Won!")
-            keepGoing = false
-        } else if (computer_score == 3) {
-            console.log("Player has lost!")
-            keepGoing = false
-        }
-
-        }
-    }
-
-
-game()
+game();
